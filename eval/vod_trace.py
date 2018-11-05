6e2e13e089718edc75f2027bd83305958e46a558
stefan-contiu <stefan.contiu@gmail.com>
2018-11-05 15:02:04 +0100

    Generated VOD trace

--- eval/vod_trace.py
+++ eval/vod_trace.py
@@ -1,8 +1,12 @@
+import csv
 import numpy as np
 import matplotlib.pyplot as plt
+import operator
 
-users_count = 220
-groups_count = 15
+users_count = 220000
+groups_count = 150000
+total_events = 9000000
+
 
 g = []
 # each group has at least a member
@@ -10,6 +14,7 @@ left_users = users_count - groups_count
 for i in range(0, groups_count):
     g.append(1)
 
+# model group popularity by zipf's law
 a = 2.2 # parameter
 id_max = 0
 for i in range(0, left_users + 1):
@@ -21,9 +26,37 @@ for i in range(0, left_users + 1):
 print("Quality of sampling : 15000 vs", id_max)
 print(g[0:15])
 
-# -------------------------------------------------------------
-total_events = 9000000
-# sample total_events from G as a discrete distribution
+# sample group add from discrete distribution
+p = []
+for i in g:
+    p.append(i / users_count)
+over_flow = sum(p) - 1
+p[0] -= over_flow
+print(sum(p))
+events = np.random.choice(groups_count, total_events, True, p)
 
-# the samples correspond to user adds
-# for each add, insert a remove operation in 30 days
+# construct trace
+trace = []
+total_ms = 11 * 7 * 24 * 3600 * 1000 # i.e. 11 weeks
+days30_ms = 30 * 24 * 3600 * 1000
+dist_add_ms = total_ms / total_events
+print("Ticks between adds :", dist_add_ms)
+for i in range(len(events)):
+    user = np.random.randint(users_count, size=1)[0]
+    trace.append((i * dist_add_ms, "a", events[i], user))
+    trace.append((i * dist_add_ms + days30_ms, "r", events[i], user))
+
+# serialize to CSV
+trace.sort(key=lambda x: x[0])
+with open('vod_trace.csv', mode='w') as trace_file:
+    trace_writer = csv.writer(trace_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    trace_writer.writerow(["tick", "operation", "group", "user"])
+    i = 0
+    for t in trace:
+        trace_writer.writerow([i, t[1], t[2], t[3]])
+        i += 1
+        if (i%100000 == 0):
+            print(i)
+
+print(events[0:30])
+print(trace[0:5])
