58567300fa1be3fccbf54e70cf578997505ccc3f
Sébastien Vaucher <seb.vaucher@gmail.com>
2018-11-08 16:49:54 +0100

    AnonymBE client: first working version

--- anonymbe-client/build.gradle.kts
+++ anonymbe-client/build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    id("org.jetbrains.kotlin.kapt") version "1.3.0"
     kotlin("jvm") version "1.3.0"
 }
 
@@ -12,7 +13,13 @@ repositories {
 }
 
 dependencies {
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.7.0")
     compile(kotlin("stdlib-jdk8"))
+
+    compile("com.squareup.retrofit2:retrofit:2.4.0")
+    compile("com.squareup.retrofit2:converter-moshi:2.4.0")
+    compile("com.squareup.okhttp3:okhttp:3.11.0")
+    implementation("com.squareup.moshi:moshi:1.7.0")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ anonymbe-client/src/main/kotlin/ch/unine/anonymbe/api/Api.kt
@@ -0,0 +1,66 @@
+package ch.unine.anonymbe.api
+
+import okhttp3.ConnectionSpec
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.X509TrustManager
+import kotlin.reflect.KClass
+
+interface AdminApi {
+    @POST("access/user")
+    fun createUser(@Body user: User): Call<UserResult>
+
+    @POST("access/group")
+    fun createGroup(@Body group: UserGroup): Call<Result>
+
+    @PUT("access/usergroup")
+    fun addUserToGroup(@Body userGroup: UserGroup): Call<Result>
+
+    @DELETE("access/usergroup")
+    fun deleteUserFromGroup(@Body userGroup: UserGroup): Call<Result>
+}
+
+interface UserApi {
+    @POST("verifier/envelope")
+    fun getEnvelope(@Body bucket: Bucket): Call<EnvelopeResult>
+}
+
+object Api {
+    fun <T : Any> build(apiType: KClass<T>, url: String = DEFAULT_URL): T {
+        val trustAllCertificatesManager = object : X509TrustManager {
+            override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) = Unit
+
+            override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) = Unit
+
+            override fun getAcceptedIssuers(): Array<X509Certificate> = emptyArray()
+        }
+
+        val okHttpClient = OkHttpClient.Builder()
+            .sslSocketFactory(SSLContext.getInstance("TLS").also {
+                it.init(null, arrayOf(trustAllCertificatesManager), SecureRandom())
+            }.socketFactory, trustAllCertificatesManager)
+            .hostnameVerifier { _, _ -> true }
+            .connectionSpecs(listOf(ConnectionSpec.RESTRICTED_TLS))
+            .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(url)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+        return retrofit
+            .create(apiType.java)
+    }
+
+    const val RESULT_OK = "OK"
+    const val RESULT_ERROR = "error"
+    private const val DEFAULT_URL = "https://hoernli-6.maas:30444/"
+}
