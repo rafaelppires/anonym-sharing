bfe836caf6f74fec89fc7f5c6ab208bfb4aa4701
Sébastien Vaucher <seb.vaucher@gmail.com>
2018-12-06 11:31:23 +0100

    Add user to group benchmark

--- anonymbe-client/src/jmh/kotlin/ch/unine/anonymbe/GroupBenchmark.kt
+++ anonymbe-client/src/jmh/kotlin/ch/unine/anonymbe/GroupBenchmark.kt
@@ -2,55 +2,35 @@ package ch.unine.anonymbe
 
 import ch.unine.anonymbe.api.User
 import ch.unine.anonymbe.api.UserGroup
+import ch.unine.anonymbe.api.throwExceptionIfNotSuccessful
 import org.openjdk.jmh.annotations.*
-import org.openjdk.jmh.infra.ThreadParams
-
-@State(Scope.Thread)
-open class GroupThreadState {
-    private var counter = 0
-    private var max = -1
-
-    fun next(): Int {
-        if (counter >= max) {
-            throw IllegalStateException("Increase number of users")
-        }
-
-        return counter++
-    }
-
-    @Setup(Level.Iteration)
-    fun setup(params: ThreadParams) {
-        val slice = GroupBenchmark.USERS_AMOUNT / params.threadCount
-        counter = params.threadIndex * slice
-        max = ((params.threadIndex + 1) * slice) - 1
-    }
-}
+import java.util.*
+import java.util.concurrent.ConcurrentLinkedQueue
 
 @State(Scope.Benchmark)
 open class GroupBenchmark : AdminBenchmark() {
+    @Param("20000")
+    private var usersAmount: String = "0"
+
+    private val queue: Queue<UserGroup> = ConcurrentLinkedQueue()
+
     @Setup(Level.Iteration)
     fun fillDatabase() {
+        println("${queue.size} items left in queue")
+        queue.clear()
         println("Filling database")
-        val users = (1..USERS_AMOUNT).map { "user$it" }
+        val users = (1..(usersAmount.toInt())).map { "user$it" }
         users.parallelStream().forEach {
             service.createUser(User(it)).execute()
+            queue.add(UserGroup(it, "creategrouptest"))
         }
         println("Filled")
     }
 
     @Benchmark
-    @BenchmarkMode(Mode.Throughput, Mode.SampleTime)
-    fun addUserToGroupBenchmark(ts: GroupThreadState) {
-        val name = "user${ts.next()}"
-        val userGroup = UserGroup(name, "group")
-        try {
-            service.addUserToGroup(userGroup).execute()
-        } catch (_: Exception) {
-            errors++
-        }
-    }
-
-    companion object {
-        const val USERS_AMOUNT = 10_000
+    @BenchmarkMode(Mode.Throughput)
+    fun addUserToGroupBenchmark() {
+        val userGroup = queue.remove()
+        service.addUserToGroup(userGroup).execute().throwExceptionIfNotSuccessful()
     }
 }
